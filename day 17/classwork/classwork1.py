c0f76fe31384eae8b02ad4bec4cb6f335388b969
giokravchenko <giokravchenko2000@gmail.com>
2025-06-10 10:52:07 +0400

    commit

---
+++ day 17/classwork/classwork1.py
@@ -0,0 +1,24 @@
+#1
+5>10 #false
+
+22+11 == 30 #false
+
+25<25 #false
+
+27<30 #true
+
+22!=30 #true
+
+22>10 #true
+
+#2
+(100>200) and (200>500) #false
+(1200 + 300 == 1500) and (20 == 20) #true
+
+bool1 = True 
+bool2 = False 
+
+bool1 or bool2 #true
+bool1 and bool1 #true
+bool2 or bool2 #false
+bool2 and bool1 # false

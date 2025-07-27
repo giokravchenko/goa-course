ea8064951ef10602d74928f10c5e63ac1d4a7f30
giokravchenko <giokravchenko2000@gmail.com>
2025-07-27 10:27:31 +0400

    commit

---
+++ day 30/classwork/classwork1.py
@@ -0,0 +1,7 @@
+i = 10
+
+while 1 <= i:
+    print(i)
+    i = i - 1
+    
+print("happy birthday!")
---
+++ day 30/homework/homework1.py
@@ -0,0 +1,10 @@
+#2)while ციკლის მეშვეობით დაბეჭდე შენი სახელი და გვარი 10 ჯერ , ხოლო როდესაც 10 ჯერ დაპრინტავს ბოლოს დაპრინტოს “დავალება შესრულებულია”
+
+real_pasword ="Giorgi2000"
+
+password =input("enter your password:")
+
+while real_pasword !=  password:
+    password =input("pasword is incorect.please enter your password agein:")
+
+print("password is corect")

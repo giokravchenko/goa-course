7b0eca48a05462fc2647dbec65762552b311d333
giokravchenko <giokravchenko2000@gmail.com>
2025-08-03 11:11:08 +0400

    commit

---
+++ day 32/classwork/classwork.py
@@ -0,0 +1,21 @@
+# real_password = "giorgi_12345678?"
+
+# user_password =input("enter your password:")
+
+# while real_password != user_password:
+#     print("password is incorect")
+#     user_password =input("enter your password again:")
+
+# print("accses grranted!")
+
+
+
+
+
+my_fiend_age = 15
+
+my_age = 10
+
+while my_age <= my_fiend_age:
+    print(my_age)
+    my_age += 1

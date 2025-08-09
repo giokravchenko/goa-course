72019ded01546c428380b9e6ca666281de94c1d6
giokravchenko <giokravchenko2000@gmail.com>
2025-08-09 10:20:30 +0400

    commit

---
+++ day 34/classwork/classwork1.py
@@ -0,0 +1,12 @@
+#1)For loop ის მეშვეობით , მომხმარებელს შემოატანინე 5 რიცხვი  და შემდეგ დაპრინტე ამ 5 რიცხვის ჯამი
+
+
+
+
+jami = 0
+for i in range(5):
+    user =(int(input("enter your number: ")))
+    jami+=user
+
+
+print(jami)
